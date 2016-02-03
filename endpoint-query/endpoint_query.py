7be8556bfaac54f27f9dbe045e70398c3ad8979c
immortalCockRoach <buptzhangyi@gmail.com>
2016-02-03 21:42:04 +0800

    init

---
+++ endpoint-query/endpoint_query.py
@@ -0,0 +1,62 @@
+import httplib, urllib
+
+QUERY_BASE_DIR = "/usr/local/etc/sparql/"
+
+class SparqlEndpoint:
+	def __init__(self, uri, port=80, dir=""):
+		self.uri = uri
+		self.port = port
+		if dir != "":
+			self.dir = dir + "/"
+		self.cache = {}
+
+	def executeQuery(self, queryname, params=None, dataset=None):
+		if queryname == None: 
+			return {"error": "no queryname provided"}
+		
+		query = Query(queryname, params, self.dir)
+
+		if dataset != None and dataset != "":
+			dataset =  "/" + dataset + "/query"
+		else:
+			dataset = "/query"
+
+		self.headers = {
+			"Content-type": "application/x-www-form-urlencoded",
+            "Accept": "application/sparql-results+json"
+        }
+        
+		self.connection = httplib.HTTPConnection(self.uri, self.port)
+		self.connection.request("POST", dataset, query.encoded(), self.headers)
+		self.response = self.connection.getresponse()
+		self.data = self.response.read()
+		self.connection.close()
+		return self.data
+
+#	def writeToCache(self, queryname, params, dataset, data):
+#		if self.cache.key
+#			self.cache[queryname].append
+
+class Query:
+	def __init__(self, name, params=None, dir=""):
+		self.name = name
+		self.params = params
+		if dir != "":
+			self.dir = dir + "/"
+		else:
+			self.dir = ""
+		self.load()
+		self.addParameters()
+
+	def load(self):
+		tmp = open(QUERY_BASE_DIR + self.dir + self.name + '.rq', 'r')
+		self.value = tmp.read()
+		tmp.close()
+
+	def addParameters(self):
+		if self.params != None:
+			for key, val in self.params.iteritems():
+				self.value = self.value.replace(key, str(val))
+
+	def encoded(self):
+		return urllib.urlencode({"query": self.value})
---
+++ endpoint-query/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='endpoint_query',
+      version='0.1',
+      py_modules=['endpoint_query'],
+      )
